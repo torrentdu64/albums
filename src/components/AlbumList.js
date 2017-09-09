0c128131a06a7536113690afb00f34da6dc14210
torrent benoit <torrentdu64@gmail.com>
2017-09-09 11:18:11 +0200

    add card details

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 
 class AlbumList extends Component {
@@ -9,11 +10,19 @@ class AlbumList extends Component {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
     .then( response => this.setState({albums: response.data }));
   }
+
+  renderAlbums(){
+    return this.state.albums.map( album =>
+      <AlbumDetail key={album.title} album={album} />
+      );
+  }
+
   render(){
     console.log(this.state);
+
     return (
       <View>
-        <Text>album list </Text>
+        {this.renderAlbums()}
       </View>
       );
   }
